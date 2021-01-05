bd3bd6c36aea149ad03988376847a844287f538c
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2021-01-05 11:56:36 +0900

    [Update] examples

--- examples/hot-reload/src/javascript/ui.js
+++ examples/hot-reload/src/javascript/ui.js
@@ -1,23 +1,23 @@
-const Ui = pc.createScript("ui");
+/*jshint esversion: 6, asi: true, laxbreak: true*/
+class Ui extends pc.ScriptType {
+  initialize() {
+    const body = document.getElementsByTagName("body")[0]
+    const head = document.getElementsByTagName("head")[0]
+    const style = `<style>${this.css.resource}</style>`
+    body.insertAdjacentHTML("afterbegin", this.html.resource)
+    head.insertAdjacentHTML("afterbegin", style)
+  }
+}
+
+pc.registerScript(Ui)
 
 Ui.attributes.add("css", {
   type: "asset",
   assetType: "css",
-  title: "CSS Asset"
-});
+  title: "CSS Asset",
+})
 Ui.attributes.add("html", {
   type: "asset",
   assetType: "html",
-  title: "HTML Asset"
-});
-
-Ui.prototype.initialize = function() {
-  const body = document.getElementsByTagName("body")[0];
-  const head = document.getElementsByTagName("head")[0];
-  const style = `<style>${this.css.resource}</style>`;
-  body.insertAdjacentHTML("afterbegin", this.html.resource);
-  head.insertAdjacentHTML("afterbegin", style);
-};
-Ui.prototype.swap = function(old) {
-  window.location.reload();
-};
+  title: "HTML Asset",
+})
